6621da5d6ee2b34e8dc1433816f2e3f401275543
Olivier Duhart <olivier.duhart@sage.com>
2022-08-02 20:46:15 +0200

    github oauth wip : almost done

--- TreeMeX/Program.cs
+++ TreeMeX/Program.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Text;
 using BackEnd;
 using Microsoft.AspNetCore.Antiforgery;
@@ -19,6 +20,8 @@ builder.Services.AddRazorPages(o => {
 builder.Services.AddScoped<INotesService>((IServiceProvider provider) =>
     new FsNotesService(@"C:\Users\olduh\DendronNotes"));
 
+builder.Services.AddSession().AddMemoryCache();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -35,6 +38,7 @@ app.UseStaticFiles();
 app.MapRazorPages();
 app.UseAuthorization();
 app.UseAuthentication();
+app.UseSession();
 
 app.Use(async (context, next) =>
 {
@@ -47,7 +51,50 @@ app.Use(async (context, next) =>
         context.Response.Headers[forge.HeaderName] = forge.RequestToken;
         await next.Invoke();
         context.Request.Body = initialBody;
+    }
+});
+
+app.Use(async (context, next) =>
+{
+    if (context.Session.TryGetValue("accessToken", out var accessToken))
+    {
         
     }
+    else
+    {
+        if (context.Request.Path == "/auth")
+        {
+            // TODO test if coming back from github => get the token => store it in session => redirect to /    
+            HttpClient client = new HttpClient();
+            var code = context.Request.Query["code"].First();
+            var state = context.Request.Query["state"].First();
+            var tokenUrl = builder.Configuration["github:tokenUrl"];
+            var clientId = builder.Configuration["github:clientId"];
+            var clientSecret = builder.Configuration["github:clientSecret"];
+            var csrf = context.Session.GetString("CSRF:State");
+            if (csrf != state)
+            {
+                context.Response.Redirect("/");
+            }
+            var query =
+                $"client_id={clientId}&client_secret={clientSecret}&code={code}&redirect_uri=https://localhost:5003/auth";
+            var token = await client.PostAsync(tokenUrl, new StringContent(query, Encoding.UTF8, "application/x-www-form-urlencoded"));
+            var body = await token.Content.ReadAsStringAsync();
+            Debug.WriteLine(body);
+            // TODO split body to token nd authorization scheme
+        }
+        else
+        {
+            var authUrl = builder.Configuration["github:authorizeUrl"];
+            var clientId = builder.Configuration["github:clientId"];
+            string csrf = "Membership.GeneratePassword(24, 1)";
+            authUrl =
+                $"{authUrl}?redirect_uri=https://localhost:5003/auth&response_type=code&client_id={clientId}&scope=repo&state={csrf}";
+
+            context.Session.SetString("CSRF:State", csrf);
+            context.Response.Redirect(authUrl);
+        }
+    }
+    await next.Invoke();
 });
 app.Run();
