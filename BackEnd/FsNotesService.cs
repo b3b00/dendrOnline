25b2a72df52dc29990c553cf1c6312c2a855cdc3
Olivier Duhart <olivier.duhart@sage.com>
2022-08-02 07:39:41 +0200

    .

--- BackEnd/FsNotesService.cs
+++ BackEnd/FsNotesService.cs
@@ -2,18 +2,29 @@ namespace BackEnd;
 
 public class FsNotesService : INotesService
 {
+    
+    public string RootDirectory { get; set; }
+    
+    public FsNotesService(string rootDirectory)
+    {
+        RootDirectory = rootDirectory;
+    }
+    
     public string GetContent(string noteName)
     {
-        throw new NotImplementedException();
+        var content = File.ReadAllText(Path.Combine(RootDirectory,"notes", noteName+".md"));
+        return content;
     }
 
     public void SetContent(string noteName, string noteContent)
     {
-        throw new NotImplementedException();
+        File.WriteAllText(Path.Combine(RootDirectory,noteName),noteContent);
     }
-
+    //C:\Users\olduh\DendronNotes
     public List<string> GetNotes()
     {
-        throw new NotImplementedException();
+        var notedir = new DirectoryInfo(Path.Combine(RootDirectory, "notes"));
+        var files = notedir.GetFiles("*.md");
+        return files.Select(x => x.Name.Replace(".md", "")).ToList();
     }
 }
