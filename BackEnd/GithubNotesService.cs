a93bcf282ca8b96239c5687eb8de9341518248df
b3b00 <olivier.duhart@sage.com>
2024-02-02 16:36:24 +0100

    delete notes : octokit.net bug #2870 workaround : implement my own REST client for DeleteFile....

--- BackEnd/GithubNotesService.cs
+++ BackEnd/GithubNotesService.cs
@@ -1,12 +1,30 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Octokit;
+using ProductHeaderValue = Octokit.ProductHeaderValue;
 
 namespace BackEnd
 {
 
+ 
+
+    public class DeleteMessage
+    {
+        [JsonPropertyName("message")]
+        public string Message { get; set; }
+       
+        [JsonPropertyName("sha")]
+        public string Sha { get; set; }
+    }
+    
     public class GithubNotesService : AsbtractNotesService
     {
         private GitHubClient gitHubClient { get; set; }
@@ -14,10 +32,6 @@ namespace BackEnd
         private long RepositoryId { get; set; }
 
         private string RepositoryName { get; set; }
-
-        private new long UserId { get; set; }
-
-        private new string UserName { get; set; }
 
         public override void SetRepository(string name, long id)
         {
@@ -146,11 +160,53 @@ This may not be a dendron repository";
                 var content = await NoteExists(noteName);
                 if (content.exists)
                 {
-                    var deleteRequest =
-                        new DeleteFileRequest($"DendrOnline : delete note {noteName}", content.content.Sha);
-                    await gitHubClient.Repository.Content.DeleteFile(RepositoryId,$"notes/{noteName}.md" , deleteRequest);
+                    await DeleteFile($"DendrOnline : delete note {noteName}", RepositoryId, $"notes/{noteName}.md",
+                        content.content.Sha);
                 }
             }
         }
+        
+        #region tooling
+        
+        private async Task DeleteFile(string message, long repositoryId, string fileName, string sha)
+        {
+            var creds = gitHubClient.Credentials;
+            var user = await gitHubClient.User.Current();
+            HttpClient client = new HttpClient();
+
+            var repo = await gitHubClient.Repository.Get(repositoryId);
+            
+            var uri = new Uri(gitHubClient.BaseAddress, $"repos/{user.Login}/{repo.Name}/contents/{fileName}");
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Delete, uri);
+            
+            DeleteMessage deleteMessage = new DeleteMessage()
+            {
+                Message = message,
+                Sha = sha
+            };
+            var jsonDeleteMessage = JsonSerializer.Serialize(deleteMessage);
+            var content = new StringContent(jsonDeleteMessage,Encoding.UTF8);
+            request.Headers.Authorization = new AuthenticationHeaderValue(AuthenticationType.Bearer.ToString(),creds.Password);
+            request.Content = content;
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            request.Headers.Add("owner",user.Login);
+            request.Headers.UserAgent.TryParseAdd("DendrOnline's agent"); 
+            var result = await client.SendAsync(request);
+            var resultContent = await result.Content.ReadAsStringAsync();
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                // TODO ? 
+            }
+            else
+            {
+                // TODO 
+            }
+            
+            
+
+        }    
+        
+        
+        #endregion
     }
 }
