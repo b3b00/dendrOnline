d7d46e924607a62c99bbcda23a17276fe2fe432c
b3b00 <olivier.duhart@sage.com>
2024-02-05 16:17:45 +0100

    remove deprecated unit tests

--- Tests/HierarchyTests.cs
+++ Tests/HierarchyTests.cs
@@ -11,47 +11,6 @@ namespace Tests;
 public class HierarchyTests
 {
 
-    [Fact]
-    public void TestNoteHierarchyJson()
-    {
-        var notesService = GetNotesService();
-        var notes = notesService.GetNotes().GetAwaiter().GetResult();
-        Check.That(notes).CountIs(5);
-        
-        var h =notesService.GetHierarchy(notes, "topic1","perso.topic1.item2",null);
-        var json = JsonSerializer.Serialize<object>(h);
-        Console.WriteLine(json);
-
-    }
-
-    [Fact]
-    public void TestNoteHierarchy()
-    {
-        var notesService = GetNotesService();
-        var notes = notesService.GetNotes().GetAwaiter().GetResult();
-        Check.That(notes).CountIs(5);
-        ;
-        var hTopic1 = notesService.GetHierarchy(notes, "topic1","perso.topic1.item2",null);
-        Check.That(hTopic1.Dump("  ")).IsEqualTo(@"  root
-    perso
-      perso.topic1
-        perso.topic1.item2 *
-        perso.topic1.item1 
-
-
-");
-            
-        var hTopic2 = notesService.GetHierarchy(notes, "topic2","perso.topic1.item1.md",null);
-        Check.That(hTopic2.Dump("  ")).IsEqualTo(@"  root
-    perso
-      perso.topic2
-        perso.topic2.item2 
-        perso.topic2.item1 
-
-
-");
-    }
-
     public static StubNotesService GetNotesService()
     {
         string rootDir = "/repository/notes/";
