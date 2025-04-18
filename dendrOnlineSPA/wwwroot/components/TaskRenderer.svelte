4386fa9f6f176387c727976d2efdb75f8ce42c76
b3b00 <olivier.duhart@sage.com>
2025-04-18 15:43:19 +0200

    task list in view mode

--- dendrOnlineSPA/wwwroot/components/TaskRenderer.svelte
+++ dendrOnlineSPA/wwwroot/components/TaskRenderer.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 
-import {onMount} from 'svelte';
+import { onMount, getContext, setContext } from 'svelte';
+import {TaskToggler} from '../scripts/TaskToggler';
+import type { ViewContext } from '../scripts/types';
+import { getDraftNote, updateNote, repository, getLoadedNote, unDraft, setTree } from '../scripts/dendronStore';
+import { DendronClient } from '../scripts/dendronClient';
+  import ErrorDialog from './ErrorDialog.svelte';
+
+
 
 export let text: string;
 
@@ -10,23 +17,46 @@ export let checked: boolean;
 
 let id: string;
 
-let  hash = async (message: string) => {
-    const data = new TextEncoder().encode(message);
-    const hashBuffer = await crypto.subtle.digest('SHA-256', data)
-    const hashArray = Array.from(new Uint8Array(hashBuffer))
-    const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('')
-    return hashHex
-  }
+const modal = getContext<Context>('simple-modal');
 
 onMount(async () => {
-    id = await hash(text);
+    id = await TaskToggler.hashItem(text);
 })
+
+
+const context = getContext<ViewContext>('view-context');
+
+let toggle = async () => {
+    checked = !checked; 
+    let noteId = context.getNoteId();
+    var note  = getLoadedNote(noteId);
+    if (note === undefined) {
+        note = getDraftNote(noteId);
+    }
+    if (note) {
+        let content = await TaskToggler.Toggle(text, id, note.body);
+        note.body = note.body = content;
+        updateNote(noteId, note);
+        const newTree = await DendronClient.SaveNote($repository.id, note);
+        if (newTree.isOk) {
+            setTree(newTree.theResult.hierarchy);
+            unDraft(noteId);
+        }
+        else {
+            modal.show(ErrorDialog, { title: 'Error', message: newTree.errorMessage });
+        }
+        
+    }
+}
+
+
+
 
 </script>
 
 {#if task}
     <li>
-        <span><input type="checkbox" checked={checked} disabled style="padding-right: 15px; display:inline"/><span><slot></slot></span></span>        
+        <span><input type="checkbox" on:change={toggle} checked={checked} style="padding-right: 15px; display:inline"/><span><slot></slot></span></span>
     </li>
 {:else}
     <li><slot></slot></li>
