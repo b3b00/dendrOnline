0122a7c8b320e8c1797648bb09c559fdb28cd9df
b3b00 <olivier.duhart@sage.com>
2025-05-09 08:08:41 +0200

    houps missing file

---
+++ dendrOnlineSPA/wwwroot/components/Asset.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+    export let asset:ImageAsset;
+
+    import { onMount, getContext, setContext } from 'svelte';
+  import { ImageAsset } from '../scripts/types';
+
+</script>
+
+<div style="display:flex;flex-direction:row;flex-wrap:wrap;align-items: center;justify-content: center;">
+    <img src={asset.url} alt={asset.name} />
+    <h3>{asset.name}</h3>
+</div>
